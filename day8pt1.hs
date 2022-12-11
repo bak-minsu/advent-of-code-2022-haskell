438d6b552624c2735023cd371b4483d8581cb1a3
Minsu Park (He/Him) <minsupark@live.com>
2022-12-10 21:57:06 -0600

    Update day8pt1.hs

--- day8pt1.hs
+++ day8pt1.hs
@@ -68,5 +68,4 @@ main = do args <- getArgs
           case args of
                [filename] -> do content  <- getFileLines filename
                                 print . countTrees isVisible $ initForestPosition content
---print . initForestPosition $ content
                _          -> putStrLn "Usage: aoc2022 <filename>"
